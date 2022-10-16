02f34b95ea405d0136881a8f3e956c3800d56c0b
ellismanellisplan <steven@ellis.co>
2022-10-15 23:17:14 -0700

    Updated app name and maximum budget error

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/tao-wallet'),
+      dir: require('path').join(__dirname, './coverage/tao-web'),
       subdir: '.',
       reporters: [
         { type: 'html' },
