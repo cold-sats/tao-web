c0be03254d6d29eb44c709688a1fcfa28c09222a
ellismanellisplan <steven@ellis.co>
2022-10-15 22:55:28 -0700

    Started on possible menu paradigm

--- src/components/top-nav/top-nav.ts
+++ src/components/top-nav/top-nav.ts
@@ -5,17 +5,7 @@ import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
   selector: 'tao-top-nav',
   encapsulation: ViewEncapsulation.None,
   styleUrls: ['./top-nav.scss'],
-  template: `
-  <img src="assets/images/tao-t-black.svg" style="height: 30px; margin-bottom: -50px; padding: 8px 5px 0px 12px">
-  <div style="background: black; width: 100%; height: 50px"></div>
-  <a [ngClass]="showBackButton ? '' : 'dont-display'" style="cursor: pointer; padding-right:380px; display: block; text-align: center; margin-top: 30px"><img style="height: 25px; width:25px" src="assets/images/icons/left-arrow-white.svg"></a>
-  <div style="display: block; text-align: center; margin-top: -17px">
-    <tao-text
-      *ngIf="title"
-      [title]="title">
-    </tao-text>
-  </div>
-  `
+  templateUrl: './top-nav.html'
 })
 
 export class TopNavComponent implements OnInit {
