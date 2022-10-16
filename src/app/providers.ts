a2c72cf8e03468a4846a2d382905a80b6e1c0c6d
ellismanellisplan <steven@ellis.co>
2022-10-15 23:43:44 -0700

    Auto-generated commit [ci skip]

--- src/app/providers.ts
+++
@@ -1,9 +0,0 @@
-import { LNMarketsProvider } from 'src/providers/ln-markets';
-import { HttpClientProvider  } from 'src/providers/http-client';
-import { UntypedFormBuilder } from '@angular/forms';
-
-export const providers = [
-  LNMarketsProvider,
-  HttpClientProvider,
-  UntypedFormBuilder
-];
