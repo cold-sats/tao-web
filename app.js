b5edf61bd17d9573eab5536adbc90d8fa66668c5
ellismanellisplan <steven@ellis.co>
2022-10-23 23:08:36 -0700

    Updated cors

--- app.js
+++ app.js
@@ -10,6 +10,13 @@ app.get('/', (req, res) => res.sendFile(path.join(__dirname)));
 app.locals.data = {};
 
 app.use(cors());
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 
 app.get('/login', async (req, res) => {
   try {
